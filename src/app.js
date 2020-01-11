592970355ebc8b2fd97ab940ef8a4837016a9c6e
Freddy Drennan <drennanfreddy@gmail.com>
2020-01-11 11:29:24 -0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,52 @@
+console.log('App.js is running');
+
+const app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of the computer',
+    options: ['One', 'Two']
+};
+
+const template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+            {/*{printOptions(app.options)}*/}
+        </ol>
+    </div>
+);
+
+const addOne = () => {
+    count++;
+    renderCounterApp();
+};
+const minusOne = () => {
+    count--;
+    renderCounterApp();
+};
+const reset = () => {
+    count = 0;
+    renderCounterApp();
+};
+
+let count = 0;
+
+const appRoot = document.getElementById('app');
+
+const renderCounterApp = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={minusOne}>-1</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    );
+
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
